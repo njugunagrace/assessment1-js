27e9a600ae2f6255590d3f573622497952c1b33e
njugunagrace <njugunagraceewambui@gamil.com>
2023-04-11 09:03:32 +0300

    first commit

---
+++ assess.js
@@ -0,0 +1,73 @@
+// Write a function that takes in a string and returns it when reversed
+let food = "eating";
+function reverseStr(food){
+return food.split('').reverse().join('')
+
+}
+let rever = reverseStr(food);
+console.log(rever);
+
+
+
+
+// Write a function that takes in the following array and consoles the target if it is found else
+// null
+let num = [2,8,0,23,5,45,76];
+Target = 23;
+function divide(num,target){
+    let left = num.slice(0,mid);
+    let right= num.slice()
+
+}
+
+
+
+// Given years between 2000 and 2023, console all the leap years in the following
+// sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
+
+for (let i = 2000; i <= 2023; i++) {
+    if (i% 4 === 0 ) {
+      console.log(i + "is a leap year" );
+    } else {
+      console.log(i + "is not a leap year" );
+    }
+
+}
+
+
+
+// Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
+// “Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
+for (i=0; i<=10;i++){
+    if(i)
+}
+
+
+
+
+
+// Write a function that takes in an array of numbers and returns an array that has all
+// elements multiplied by 4.
+let numArray = [12,87,45,75,23,64,73];
+function numbArr(){
+   let i = 0;
+   while(i< numArray.length){
+    console.log(numArray[i]*4)
+    i++
+   }
+   
+}
+numbArr()
+
+// Write a function that takes in an array of strings and returns an array with every element
+// turned into a number
+let nums = ["10","24","45","56","67"];
+ function numbers(nums){
+    nums.toString
+ }
+ console.log(nums)
+ numbers(nums);
+
+
+
+
